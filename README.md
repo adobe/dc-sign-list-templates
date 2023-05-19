e034264ffa65a21c9d93470819f34d6e2a27b385
Chris Hughes <cajhughes@gmail.com>
2023-05-19 13:26:35 +0100

    First draft of aas-list-templates command-line tool

---
+++ README.md
@@ -0,0 +1,66 @@
+# aas-list-templates
+
+## Introduction
+
+This Java-based command-line tool provides a simple method to list all the library templates within one [Adobe Acrobat Sign](https://www.adobe.com/sign.html) account.
+
+## Set-Up Instructions
+
++ Download the latest release of the `aas-list-templates-<version>.jar` from the [Releases](https://git.corp.adobe.com/sign-acs/aas-list-templates/releases) page
++ [Download Java 1.8](https://www.oracle.com/java/technologies/javase/javase8-archive-downloads.html), if you don't already have it installed on your machine
++ [IP Addresses to add to your allow list](https://helpx.adobe.com/sign/system-requirements.html#IPs), if needed
+
+As the tool makes use of the [Adobe Sign REST API](https://secure.adobesign.com/public/docs/restapi/v6), it is necessary to [provide an integration key](https://helpx.adobe.com/sign/kb/how-to-create-an-integration-key.html) for your account.
+
+To do this, follow the steps below (for each account):
+
+1. Log in to your Acrobat Sign account (as an Account Administrator)
+2. Click **Account** from the menu
+3. Type "Access tokens" in the search field on the left side of the screen
+4. Press the "+" icon on the right side
+5. Create a key with the scopes needed (`user_read` and `library_read` is required for your account)
+6. Double-click the key you just created and copy the FULL text (it goes off-screen to the right so make sure you get it all)
+7. Store the `Integration Key` for later use
+
+## Usage
+
++ Open a command prompt in the directory where you have the JAR file downloaded
++ Execute the following command, replacing:
+  + `<version>` with the appropriate value for the release, such as `1.0.0`
+  + `<integrationKey>` with your saved value from above
+
+```sh
+java -jar aas-list-templates-<version>.jar <integrationKey>
+```
+
+Assuming you have specified the required parameters correctly, then you should see output similar to that below:
+
+![Sample Output](/images/example-usage.png)
+
+Note that the output is deliberately concise to support redirecting output to a CSV file which can then be opened in programs such as Microsoft Excel.
+
+```sh
+java -jar aas-list-templates-<version>.jar <integrationKey> > output.csv
+```
+
+## Building
+
+If you would like to build a release package locally, you should have the following software installed:
+
++ OS: Linux, macOS, Windows
++ Java JDK: version 1.8 or above
++ Apache Maven
+
+The tool makes use of the [Adobe Sign Java SDK](https://opensource.adobe.com/acrobat-sign/sdks/java.html), so you will first need to clone the [acrobat-sign repository](https://github.com/adobe/acrobat-sign) and install the package into your Maven repository by issuing the following commands:
+
+```sh
+cd <location-of-cloned-acrobat-sign-repo>/sdks/AcrobatSign_JAVA_SDK
+mvn clean install
+```
+
+Once this has completed, clone this repository and build the package by issuing:
+
+```sh
+cd <location-of-cloned-tool-repo>
+mvn clean package
+```
---
+++ src/main/java/uk/org/esig/adobe/libraryDocuments/ListTemplates.java
@@ -0,0 +1,143 @@
+package uk.org.esig.adobe.libraryDocuments;
+
+import io.swagger.client.api.BaseUrisApi;
+import io.swagger.client.api.LibraryDocumentsApi;
+import io.swagger.client.api.UsersApi;
+import io.swagger.client.model.ApiClient;
+import io.swagger.client.model.ApiException;
+import io.swagger.client.model.baseUris.BaseUriInfo;
+import io.swagger.client.model.libraryDocuments.LibraryDocument;
+import io.swagger.client.model.libraryDocuments.LibraryDocuments;
+import io.swagger.client.model.users.UserInfo;
+import io.swagger.client.model.users.UsersInfo;
+import org.apache.commons.csv.CSVFormat;
+
+import java.util.List;
+
+public class ListTemplates {
+    private static final String API_HOST = "https://api.adobesign.com/";
+    private static final String API_PATH = "api/rest/v6";
+    private static final String API_USER_PREFIX = "email:";
+    private static final String BEARER = "Bearer ";
+    private static final int PAGE_SIZE = 10;
+    private static final int TIMEOUT = 300000;
+    private static final String USAGE = "Usage: java -jar aas-list-templates-<version>.jar <integrationKey>";
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println(getUsage());
+        }
+        else {
+            String accessToken = BEARER + args[0];
+            ListTemplates list = new ListTemplates();
+            try {
+                list.execute(accessToken);
+            }
+            catch (ApiException ae) {
+                System.out.println(getExceptionDetails(ae));
+                ae.printStackTrace();
+            }
+        }
+    }
+
+    public void execute(String accessToken) throws ApiException {
+        /*
+         *  Establish connection to Adobe Sign API, and obtain the correct API Access Point for the account
+         */
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(API_HOST + API_PATH);
+        apiClient.setConnectTimeout(TIMEOUT).setReadTimeout(TIMEOUT);
+        BaseUrisApi baseUrisApi = new BaseUrisApi(apiClient);
+        BaseUriInfo baseUriInfo = baseUrisApi.getBaseUris(accessToken);
+        apiClient.setBasePath(baseUriInfo.getApiAccessPoint() + API_PATH);
+
+        /*
+         *  Instantiate APIs for source and destination accounts
+         */
+        UsersApi usersApi = new UsersApi(apiClient);
+        LibraryDocumentsApi libraryDocumentsApi = new LibraryDocumentsApi(apiClient);
+
+        /*
+         *  Obtain the first page of users
+         */
+        UsersInfo usersInfo = usersApi.getUsers(accessToken, null, null, PAGE_SIZE);
+        List<UserInfo> userInfoList = usersInfo.getUserInfoList();
+        System.out.println(format("template_id", "template_name", "owner_email"));
+        while (userInfoList != null && !userInfoList.isEmpty()) {
+            /*
+             *  For each user:
+             *  (a) Get the list of templates they have access to
+             *  (b) Check which of these the user owns
+             *  (c) Output details if they are the owner
+             */
+            for (UserInfo userInfo: userInfoList) {
+                String email = userInfo.getEmail();
+                String apiUser = API_USER_PREFIX + email;
+                LibraryDocuments libraryDocuments = libraryDocumentsApi.getLibraryDocuments(accessToken,
+                                                                                            apiUser,
+                                                                                            null,
+                                                                                            Boolean.FALSE,
+                                                                                            null,
+                                                                                            PAGE_SIZE);
+                List<LibraryDocument> libraryDocumentList = libraryDocuments.getLibraryDocumentList();
+                while (libraryDocumentList != null && !libraryDocumentList.isEmpty()) {
+                    for (LibraryDocument libraryDocument: libraryDocumentList) {
+                        String owner = libraryDocument.getOwnerEmail();
+                        String id = libraryDocument.getId();
+                        String name = libraryDocument.getName();
+                        if (email != null && email.equals(owner)) {
+                            System.out.println(format(id, name, owner));
+                        }
+                    }
+                    String libraryDocumentCursor = libraryDocuments.getPage().getNextCursor();
+                    if (libraryDocumentCursor != null && !libraryDocumentCursor.isEmpty()) {
+                        libraryDocuments = libraryDocumentsApi.getLibraryDocuments(accessToken,
+                                                                                   apiUser,
+                                                                                   null,
+                                                                                   Boolean.FALSE,
+                                                                                   libraryDocumentCursor,
+                                                                                   PAGE_SIZE);
+                        libraryDocumentList = libraryDocuments.getLibraryDocumentList();
+                    }
+                    else {
+                        libraryDocumentList = null;
+                    }
+                }
+            }
+            String userCursor = usersInfo.getPage().getNextCursor();
+            if (userCursor != null && !userCursor.isEmpty()) {
+                usersInfo = usersApi.getUsers(accessToken, null, userCursor, PAGE_SIZE);
+                userInfoList = usersInfo.getUserInfoList();
+            }
+            else {
+                userInfoList = null;
+            }
+        }
+    }
+
+    private String format(String id, String name, String email) {
+        return CSVFormat.EXCEL.format(id, name, email);
+    }
+
+    private static String getExceptionDetails(ApiException e) {
+        StringBuilder sb = new StringBuilder();
+        if (e != null) {
+            sb.append("Message: ");
+            sb.append(e.getMessage());
+            sb.append("\n");
+            sb.append("Code: ");
+            sb.append(e.getCode());
+            sb.append("\n");
+            sb.append("Response Headers: ");
+            sb.append(e.getResponseHeaders());
+            sb.append("\n");
+            sb.append("Response Body: ");
+            sb.append(e.getResponseBody());
+        }
+        return sb.toString();
+    }
+
+    private static String getUsage() {
+        return USAGE;
+    }
+}
